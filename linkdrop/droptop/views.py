7dc3bd2b0ef5c0e672f0880435fbe130e85dac79
Justinas Ulevicius <justinas.ulevicius@gmail.com>
2017-01-25 22:08:33 +0000

    Starting user auth.

--- linkdrop/droptop/views.py
+++ linkdrop/droptop/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+form django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+	"""
+	Index page once logged in.
+	Contains functionality to drop a link.
+	"""
+
+	
+
